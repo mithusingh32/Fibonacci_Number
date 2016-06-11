0cafd6bac30b01684b0ede16540421a4db52d4d9
mithusingh32 <mithusingh32@gmail.com>
2016-06-11 15:34:04 -0400

    Update Fibonacci_Number.py

--- Fibonacci_Number.py
+++ Fibonacci_Number.py
@@ -1,16 +1,15 @@
 #Print out the Fiboancci number at any location in the sequnce
+#You also can make it so the entire squence is reutrned
 
 def fibNum(howManyPlaces):
-    #Fibancci Number
     ls = [0,1]
     first =0
     
     while first < howManyPlaces-1:
         i = len(ls)
-        #print ">>> length of ls %d" %i
         nextValue = ls[i-1] + ls[i-2]
         ls.append(nextValue)
-        #print ">>> List LS %r" %ls
         first += 1
         
+    #return ls
     return int(ls[howManyPlaces])
